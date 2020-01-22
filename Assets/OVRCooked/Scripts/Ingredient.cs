287639522a4ea0d92b7079b8441819062b07b214
Gonzalo Piffaretti <gpiffaretti@gmail.com>
2020-01-22 23:58:35 +0100

    Made the pot empty in the inside, can drop elements inside

---
+++ Assets/OVRCooked/Scripts/Ingredient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    [SerializeField]
+    IngredientType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum IngredientType 
+{
+    Onion
+}
