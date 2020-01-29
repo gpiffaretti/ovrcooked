0164b1723470abed230b7fa423b2f4ed0335c519
Albert Rodriguez Franco <albertrfranco@gmail.com>
2020-01-29 01:59:30 +0100

    OrderManagement 1st iteration

---
+++ Assets/OVRCooked/Scripts/UI/OrderUIManager.cs
@@ -0,0 +1,46 @@
+﻿using Assets.OVRCooked.Scripts.Orders;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.OVRCooked.Scripts.UI
+{
+    public class OrderUIManager : Singleton<OrderUIManager>
+    {
+        [SerializeField]
+        OrderDisplay orderDisplayPrefab;
+
+        [SerializeField]
+        GameObject ordersParent;
+
+        List<OrderDisplay> activeOrders = new List<OrderDisplay>();
+
+        private void Start()
+        {
+            OrderManager.Instance.OrderCreated += OnOrderCreated;
+        }
+
+        private void OnOrderCreated(Order order)
+        {
+            // Instantiate OrderDisplay with proper parameter
+            order.OrderExpired += OnOrderExpired;
+            var orderDisplay = Instantiate<OrderDisplay>(orderDisplayPrefab, ordersParent.transform);
+            orderDisplay.order = order;
+            orderDisplay.Initialize(order);
+
+            activeOrders.Add(orderDisplay);
+        }
+
+        private void OnOrderExpired(Order order)
+        {
+            var activeOrder = activeOrders.Single(x => x.order == order);
+            activeOrders.Remove(activeOrder);
+            Destroy(activeOrder.gameObject);
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
