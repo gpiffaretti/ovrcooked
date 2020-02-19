c587dd0c9b2f4ae2fb01dd85cd74b501baff5765
Gonzalo Piffaretti <gpiffaretti@gmail.com>
2020-02-19 20:29:39 +0100

    ClockUI

---
+++ Assets/OVRCooked/Scripts/UI/ClockUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockUI : MonoBehaviour
+{
+
+    [SerializeField]
+    TimeBar timeBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameManager gameManger = FindObjectOfType<GameManager>();
+        //gameManger.GameStarted += OnGameStartedHandler;
+        //gameManger.GameEnded += OnGamePausedHandler;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGameStartedHandler() 
+    {
+        // init timebar
+    }
+
+    void OnGameEndedHandler()
+    {
+        // init timebar
+    }
+}
